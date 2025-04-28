612224477b30300ff3de418015b2df7bb9e49e01
F3rn4ndo07 <josesaicoo20@gmail.com>
2025-04-27 22:50:07 -0500

    Queries para Pagos(Listar total de importes por tipo de moneda y Listar pagos prontos a vencer)

---
+++ src/main/java/com/geovivienda/geovivienda/dtos/PagosCercanosDTO.java
@@ -0,0 +1,20 @@
+package com.geovivienda.geovivienda.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Setter
+@Getter
+public class PagosCercanosDTO {
+    private Integer idPago;
+    private Instant fechaVencimiento;
+    private String inmueble;
+    private BigDecimal importe;
+    private String tipoMoneda;
+    private String comprador;
+    private String vendedor;
+
+}
