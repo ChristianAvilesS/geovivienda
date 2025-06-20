fa054f091a837d5e7c6ca050a75e9ac1ab4880d5
Piwui <aldair6548151123@gmail.com>
2025-06-19 21:39:15 -0500

    Actualizacion menor para el bot.

--- src/main/java/com/geovivienda/geovivienda/controllers/ChatController.java
+++ src/main/java/com/geovivienda/geovivienda/controllers/ChatController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "http://localhost:4200") // o el dominio de tu frontend
+@CrossOrigin(origins = "http://localhost:4200")
 public class ChatController {
 
     private final OpenRouterServiceImpl chatService;
--- src/main/java/com/geovivienda/geovivienda/services/implementations/OpenRouterServiceImpl.java
+++ src/main/java/com/geovivienda/geovivienda/services/implementations/OpenRouterServiceImpl.java
@@ -17,15 +17,15 @@ import java.util.Map;
 public class OpenRouterServiceImpl {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String API_KEY = "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";
+    private final String API_KEY = "Aqui debes colocar tu API Key de OpenRouter (https://openrouter.ai/keys)";
     private final String URL = "https://openrouter.ai/api/v1/chat/completions";
 
     public String askOpenRouter(List<ChatMessageDTO> messages) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer " + API_KEY);
-        headers.set("HTTP-Referer", "https://localhost:4200"); // Requerido por OpenRouter
-        headers.set("X-Title", "MiAppChatbot");              // Título personalizado (opcional)
+        headers.set("HTTP-Referer", "https://localhost:4200");
+        headers.set("X-Title", "ChatBotEva");
 
         Map<String, Object> request = new HashMap<>();
         request.put("model", "openai/gpt-3.5-turbo");
